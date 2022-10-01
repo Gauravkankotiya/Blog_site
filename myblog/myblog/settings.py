deaf97f29de882e17285d0f5a581865942527ad0
Gaurav_Kankotiya <gauravkankotiya99@gmail.comm>
2022-10-01 19:46:37 +0530

    second

--- myblog/myblog/settings.py
+++ myblog/myblog/settings.py
@@ -12,8 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
-import django_heroku
-import dj_database_url
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -139,4 +138,3 @@ LOGIN_REDIRECT_URL = '/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-django_heroku.settings(locals())
